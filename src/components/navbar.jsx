841a06c3a1511891f7d9aee0dcd647c7e05d2267
Sameer Chawla <sameerchawla@Sameers-MacBook-Air.local>
2023-09-23 13:39:12 -0400

    commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function Nav() {
+    return (
+        <div class = "navbar">
+            <h1>Explore</h1>    
+        </div>
+    )
+}
+export default Nav;
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Nav from "./components/nav.jsx"
-
 function Dashbaord() {
     
 }
