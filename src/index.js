5e5415bf7d42ccfa21c84af8ff6e43edc7a17a1d
Declan <decavacado@gmail.com>
2023-09-23 13:23:00 -0400

    Add: Pages + React Router

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
+import Dashboard from "./pages/dashboard"
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>
+    },
+    {
+        path: "/dashboard",
+        element: Dashboard
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <RouterProvider router={router}>
+    </RouterProvider>
+
 );
 
---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Nav from "./components/nav.jsx"
+
+function Dashbaord() {
+    
+}
