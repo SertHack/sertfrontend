44d6321ff658a3ad613c8c8cdafe719304f4fb8f
Varun Rao <varunvellorerao@gmail.com>
2023-09-24 02:57:53 -0400

    Added more explore posts

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -13,8 +13,6 @@ function Explore() {
         <body>
         <div id="explore">
         <Navbar></Navbar>
-        <p>Explore Page</p>
-        <Footer></Footer>
     </div>
     <div class="explore-list-container">
 
@@ -23,21 +21,55 @@ function Explore() {
         <div class="explore">
 
             <div class="post">
-                <a href="../pages/profile.jsx"><h3 class="User has-text-link">Jai Patel</h3></a>
-                {/*<img src={postimage} alt="LaTao Hot Pot" />*/}
-                <div class="caption has-text-white">
+                <a href="../pages/profile.jsx"><h3 class="User has-text-link ml-3">Jai Patel</h3></a>
+                <div class="caption has-text-white ml-3">
                     I just checked out this amazing restaurant called LaTao Hot Pot
                     in College Park, MD, right outside the University of Maryland
-                    campus.
+                    campus! Come support their business!
                 </div>
                 {/*<a href="#" class="details-btn">More Details</a>*/}
-                <span class="post-date">2:34 PM 09/24/2023</span>
+                <span class="post-date ml-4">2:34 PM 09/24/2023</span>
+            </div>
+
+            <div class="post">
+                <a href="../pages/profile.jsx"><h3 class="User has-text-link ml-3">Sameer Chawla</h3></a>
+                <div class="caption has-text-white ml-3 mr-4">
+                    Over the summer, I completed an internship at Google as part of the
+                    STEP internship program in California. I am so honored to have met
+                    so many great people throughout this experience and I am proud to
+                    announce that I have a job offer after I graduate!
+                </div>
+                {/*<a href="#" class="details-btn">More Details</a>*/}
+                <span class="post-date ml-4">11:19 AM 09/22/2023</span>
+            </div>
+
+            <div class="post">
+                <a href="../pages/profile.jsx"><h3 class="User has-text-link ml-3">Varun Rao</h3></a>
+                <div class="caption has-text-white ml-3 mr-4">
+                    Are you interested in building a Flask application to include on your
+                    resume and project portfolio? If so, please Sertwork with me and I can help
+                    you out for a small hourly fee.
+                </div>
+                {/*<a href="#" class="details-btn">More Details</a>*/}
+                <span class="post-date ml-4">1:56 PM 09/21/2023</span>
+            </div>
+
+            <div class="post">
+                <a href="../pages/profile.jsx"><h3 class="User has-text-link ml-3">Jai Patel</h3></a>
+                <div class="caption has-text-white ml-3 mr-4">
+                    To all CS and Computer Engineering majors: Northrop Grumman will
+                    be at the Iribe Center from 1PM-3:30PM. If you are interested
+                    in interviewing for an internship or entry-level position,
+                    remember to bring your resume and ask the recruiters some questions!
+                </div>
+                {/*<a href="#" class="details-btn">More Details</a>*/}
+                <span class="post-date ml-4">12:01 PM 09/19/2023</span>
             </div>
 
         </div>
 
         <div id="refresh">
-            <a href="#" class="refresh-btn">Load more posts</a>
+            <a href="window.location.reload()"class="refresh-btn" onclick="window.location.reload()">Load more posts</a>
         </div>
 
     </div>
@@ -47,6 +79,7 @@ function Explore() {
 
         </p>
     </div>
+    <Footer></Footer>
     </body>
     );
 }
