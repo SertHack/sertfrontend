2debd73f8e146233cfe5f6d828de922c34ba158a
Varun Rao <varunvellorerao@gmail.com>
2023-09-23 23:32:14 -0400

    Jobs and jobs Css file

--- src/pages/jobs.jsx
+++ src/pages/jobs.jsx
@@ -2,13 +2,90 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Navbar from "../components/navbar";
+import '../css/jobs.css'
+import logo from '../Images/SertLogoReduced.svg'
 
 function Jobs() {
     return (
-    <div id="jobs">
+    <body>
+        <div id="jobs">
         <Navbar></Navbar>
         <p>Jobs Page</p>
     </div>
+    <div class="jobs-list-container">
+        <h2>5 Jobs</h2>
+
+        <div class="jobs">
+
+            <div class="job">
+                <img src={logo} alt="Company logo" />
+                <h3 class="job-title has-text-link">Software Engineer</h3>
+                <div class="details has-text-white">
+                    Responsible for designing, developing, and maintaining software
+                    systems and applications for the company.
+                </div>
+                <a href="#" class="details-btn">More Details</a>
+                <span class="open-positions">Position Open</span>
+            </div>
+
+            <div class="job">
+                <img src={logo} alt="Company logo" />
+                <h3 class="job-title has-text-link">Software Engineer</h3>
+                <div class="details has-text-white">
+                    Responsible for designing, developing, and maintaining software
+                    systems and applications for the company.
+                </div>
+                <a href="#" class="details-btn">More Details</a>
+                <span class="open-positions">Position Open</span>
+            </div>
+
+            <div class="job">
+                <img src={logo} alt="Company logo" />
+                <h3 class="job-title has-text-link">Software Engineer</h3>
+                <div class="details has-text-white">
+                    Responsible for designing, developing, and maintaining software
+                    systems and applications for the company.
+                </div>
+                <a href="#" class="details-btn">More Details</a>
+                <span class="open-positions">Position Open</span>
+            </div>
+
+            <div class="job">
+                <img src={logo} alt="Company logo" />
+                <h3 class="job-title has-text-link">Software Engineer</h3>
+                <div class="details has-text-white">
+                    Responsible for designing, developing, and maintaining software
+                    systems and applications for the company.
+                </div>
+                <a href="#" class="details-btn">More Details</a>
+                <span class="open-positions">Position Open</span>
+            </div>
+
+            <div class="job">
+                <img src={logo} alt="Company logo" />
+                <h3 class="job-title has-text-link">Software Engineer</h3>
+                <div class="details has-text-white">
+                    Responsible for designing, developing, and maintaining software
+                    systems and applications for the company.
+                </div>
+                <a href="#" class="details-btn">More Details</a>
+                <span class="open-positions">Position Open</span>
+            </div>
+
+            <div class="job">
+                <img src={logo} alt="Company logo" />
+                <h3 class="job-title has-text-link">Software Engineer</h3>
+                <div class="details has-text-white">
+                    Responsible for designing, developing, and maintaining software
+                    systems and applications for the company.
+                </div>
+                <a href="#" class="details-btn">More Details</a>
+                <span class="open-positions">Position Open</span>
+            </div>
+
+        </div>
+    </div>
+    </body>
     );
 }
 
