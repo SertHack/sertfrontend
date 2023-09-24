bb26b89d69212f5e4258aa4dcfcd08ba5b67e25f
Jai Patel <mitjaipp@gmail.com>
2023-09-24 00:56:23 -0400

    Network initial commit

--- src/pages/network.jsx
+++ src/pages/network.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Footer from "../components/footer"
-import Navbar from "../components/navbar";
+import React, { useEffect, useRef } from 'react';
+import { Network } from 'vis-network';
+import Navbar from '../components/navbar';
 
-function Network() {
-    return (
+const Graph = () => {
+  const container = useRef(null);
+
+  const nodes = [
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+    { id: 4, label: 'Node 4' },
+    { id: 5, label: 'Node 5' }
+  ];
+
+  const edges = [
+    { from: 1, to: 3 },
+    { from: 1, to: 2 },
+    { from: 2, to: 4 },
+    { from: 2, to: 5 },
+    { from: 3, to: 3 }
+  ];
+
+  const options = {};
+
+  useEffect(() => {
+    const network =
+      container.current &&
+      new Network(container.current, { nodes, edges }, options);
+  }, [container, nodes, edges]);
+
+  return (
     <div id="network">
         <Navbar></Navbar>
-        <p>Network Page</p>
-        <Footer></Footer>
+		<div ref={container} style={{ height: '500px', width: '800px' }} />
     </div>
-    );
-}
+  );
+};
 
-export default Network;
+export default Graph;
