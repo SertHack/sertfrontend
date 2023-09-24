c7b3ee83cd423a47f52b55c70407fcc4d81d2436
Jai Patel <mitjaipp@gmail.com>
2023-09-24 06:20:12 -0400

    Updates post using explore

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Navbar from "../components/navbar.jsx"
 import Footer from "../components/footer"
-import '../css/explore.css'
+import '../css/explore.css';
 import usericon from '../Images/Profile.jpeg'
-import postimage from '../Images/latao.jpeg'
 
-function Post() {
+function Post(data) {
+    data = data.data;
+    const time = new Date(data.time);
     return (
-        <body>
+        <div class="post" style={{ marginBottom: '.8em'}}>
             <div class="post">
-                <div class="caption has-text-white">
+                <img src={usericon} style={{ width: '8%' }} alt="User Profile Picture" />
+                <h3 class="User has-text-link">{data.name}</h3>
+                <div class="caption" style={{ color: 'white' }}>
+                    {data.caption}
                 </div>
+                {/* <span class="post-date">2:34 PM 09/24/2023</span> */}
+                <span class="post-date">{`${time.getTime()}:${time.getMinutes()} PM ${time.getDay()}/${time.getMonth()}/${time.getFullYear()}`}</span>
             </div>
-        </body>
+        </div>
     );
 }
 
