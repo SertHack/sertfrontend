b876d2caf4129e645e6522f95960272134a7d82e
Sameer Chawla <sameerchawla@Sameers-MacBook-Air.local>
2023-09-23 20:40:26 -0400

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom"
 import ReactDOM from 'react-dom/client';
-
+import logo from '../Images/SertLogoReduced.svg'
+import css from '../css/navbar.css'
 
 function Nav() {
     return (
-        <div id="navbar">
-            <Link to="/">Dashboard</Link>
+        <div class="navbar has-text-warning" id="navbar">
+            <div class ="navbar-brand" href = "/dashboard">
+                <img class="navbar-item" src={logo} width="100" height="100"></img>
+            </div>
             <Link to="/explore">Explore</Link>
             <Link to="/network">Network</Link>
             <Link to="/jobs">Jobs</Link>
